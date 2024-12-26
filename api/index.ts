3eb61443833c6c0bf3326b30fde22145832ea89c
webdev-ls <126141454+webdev-ls@users.noreply.github.com>
2024-12-26 14:27:04 +0530

    debug

--- api/index.ts
+++ api/index.ts
@@ -4,21 +4,20 @@ import html2json from "./html2json";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { fileUrl } = req.query;
-  return res.json({ fileUrl });
-  // try {
-  //   const docxhtml  = await docx2html(fileUrl);
-  //   if(docxhtml && docxhtml.html){
-  //     const docxjson = html2json(docxhtml.html);
-  //     return res.json({ docxjson, docxhtml });
-  //   }else{
-  //     throw new Error("Error Parsing Html");
-  //   }
-  // } catch (error) {
-  //   return new Response(JSON.stringify({ error: error.message }), {
-  //     status: 400,
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //   });
-  // }
+  try {
+    const docxhtml  = await docx2html(fileUrl);
+    if(docxhtml && docxhtml.html){
+      const docxjson = html2json(docxhtml.html);
+      return res.json({ docxjson, docxhtml });
+    }else{
+      throw new Error("Error Parsing Html");
+    }
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 400,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
 }
